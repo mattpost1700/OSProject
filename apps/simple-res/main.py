e30ade3db431ff13ce8c1622976eddd1376ef3da
mattpost1700 <72893809+mattpost1700@users.noreply.github.com>
2023-04-06 20:08:25 -0400

    no charts

--- apps/simple-res/main.py
+++ apps/simple-res/main.py
@@ -1,6 +1,7 @@
+import datetime
 import time
 
 if __name__ == "__main__":
     while True:
-        print(f"I am running!")
+        print(f"I am running! ({datetime.datetime.now()})")
         time.sleep(10)
